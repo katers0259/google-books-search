904d12464876c7dc64afbff4e3ee0c2a8762ff32
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-02-18 21:19:13 -0700

    linking in css for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import './Header.css'
 
 
 function Header() {
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col > <header>
+             <header>
                         <h1> Books</h1>
                     </header>
-                    </Col>
-                </Row>
-            </Container>
+               
         </div>
     )
 }
